1d6e64405830d0e09528c1a51968fbf739c0576f
supremesupreme <91563714+supremesupreme@users.noreply.github.com>
2024-11-04 11:55:58 -0500

    Added author function + tests

---
+++ lib/aoi.js/src/functions/discord/author/$username.ts
@@ -0,0 +1,75 @@
+import FunctionBuilder from '@aoi.js/core/builders/Function.js';
+import { parseString } from '@aoi.js/core/parsers/string.js';
+import { ErrorCode, FunctionType, ReturnType } from '@aoi.js/typings/enum.js';
+import { escapeResult } from '@aoi.js/utils/Helpers/core.js';
+import AoiError from '@aoi.js/core/Error.js';
+
+/**
+ * Returns the username of the user.
+ * @example
+ * ```aoi
+ * ---
+ * name: username
+ * type: basic
+ * ---
+ *
+ * $username // returns the username of the current user
+ * $username[userid] // returns the username of the specified user
+ * ```
+ */
+
+const $username = new FunctionBuilder()
+	.setName('$username')
+	.setType(FunctionType.Getter)
+	.setReturns(ReturnType.String | ReturnType.Void)
+	.setBrackets(true)
+	.setOptional(true)
+	.setFields([
+		{
+			name: 'userId',
+			type: ReturnType.String,
+			required: false,
+			description: 'UserId to search for',
+		},
+	])
+	.setCode((data, scopes, thisArg) => {
+		const [userId] = thisArg.getParams(data);
+
+		let res;
+		if (!userId) {
+			res = thisArg.getResultString(
+				(discordData) => discordData.author?.username,
+			);
+			const escaped = escapeResult(res);
+			return {
+				code: escaped,
+				scope: scopes,
+			};
+		}
+
+		let parsedUserId = thisArg.parseData(userId, ReturnType.Number);
+
+		if (!thisArg.isCorrectType(parsedUserId, ReturnType.Number)) {
+			throw AoiError.FunctionError(
+				ErrorCode.InvalidArgumentType,
+				`Invalid type for parameter 'userid' in function $username, got ${parsedUserId} expected: number.`,
+				data,
+			);
+		}
+
+		res = thisArg.getResultString(
+			async (discordData) =>
+				(await discordData.client.users.fetch('$0'))?.username,
+			[parseString(userId)],
+		);
+
+		const escaped = escapeResult(res);
+
+		return {
+			code: escaped,
+			scope: scopes,
+		};
+	})
+	.build();
+
+export { $username };
